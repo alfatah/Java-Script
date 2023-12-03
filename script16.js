85450523097791adf0eca12446c6d2e3aa7710c6
Sholahuddin Alfatah <sholahuddin1999@gmail.com>
2023-12-03 12:30:00 +0700

    update

--- script16.js
+++ script16.js
@@ -3,4 +3,25 @@
 // alert('Saya');
 // alert('Sholahuddin Alfatah');
 
-prompt('masukkan nama:');
+// var nama = prompt('masukkan nama:');
+// alert(nama);
+
+// var tes = confirm('kamu yakin?');
+// alert(tes);
+// if (tes === true) {
+//     alert('user menakan OK!');
+// } else {
+//     alert('user menakan CANCEL!');
+// }
+
+alert('selamat datang..');
+var lagi = true;
+
+while( lagi === true ) {
+    var nama = prompt('masukkan nama:');
+    alert('halo ' + nama);
+
+    lagi = confirm('coba lagi?');
+}
+
+alert('terima kasih..');
