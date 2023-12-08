6327f0a2dc6b530ecd71a82c0d0508d5dfce178d
Sholahuddin Alfatah <sholahuddin1999@gmail.com>
2023-12-08 16:24:12 +0700

    update

--- while.js
+++ while.js
@@ -17,9 +17,23 @@
 //     console.log('Hello world!');
 // }
 
+// var ulang = true;
+// while(ulang) {
+//     console.log('Hello World!');   
+//     ulang = confirm('lagi?');
+// }
 
-var ulang = true;
-while(ulang) {
-    console.log('Hello World!');   
-    ulang = confirm('lagi?');
-}
+// var nilaiAwal = 1;
+// while(nilaiAwal <= 100) {
+//     console.log('Hello World ' + nilaiAwal + 'x');
+// nilaiAwal++;
+// } 
+
+var nilaiAwal = 1;
+while(nilaiAwal <= 10) {
+    console.log('Angkot No. ' + nilaiAwal + ' beroperasi dengan baik.');
+nilaiAwal++;
+} 
+
+
+
