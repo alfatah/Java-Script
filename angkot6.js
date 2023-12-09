e276c7eff4e3f3e77c28b6b0734e5c23b8177fac
Sholahuddin Alfatah <sholahuddin1999@gmail.com>
2023-12-09 20:15:45 +0700

    update

--- angkot6.js
+++ angkot6.js
@@ -3,9 +3,9 @@ var angkotBeroperasi = 6;
 
 for( var noAngkot = 1; noAngkot <= jmlAngkot; noAngkot++) {
 
-    if( noAngkot <= 6) {
+    if( noAngkot <= 6 && noAngkot !== 5 ) {
         console.log('Angkot No. ' + noAngkot + ' beroperasi dengan baik.');
-    } else if( noAngkot === 8 ) {
+    } else if( noAngkot === 8 || noAngkot === 10 || noAngkot === 5) {
         console.log('Angkot No. ' + noAngkot + ' sedang lembur');
     } else {
         console.log('Angkot No. ' + noAngkot + ' sedang tidak beroperasi');
