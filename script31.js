6f5e2d4024024d922b3772e964973385ad9a58c4
Sholahuddin Alfatah <sholahuddin1999@gmail.com>
2023-12-10 13:50:35 +0700

    update

---
+++ script31.js
@@ -0,0 +1,17 @@
+// global scope / window scope
+// var a = 1;
+
+// "use strict";
+/////////////////////////////////////////
+var a = 1;
+
+function tes(a) {
+    console.log(a);
+}
+
+tes(a);
+console.log(a);
+/////////////////////////////////////////
+
+
+
